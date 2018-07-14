9d27a90f18de60db1c77b3cb4e16408015164e33
NitinGahlawat11 <webnitin640@gmail.com>
2018-07-14 18:21:33 +0530

    basic dashboard prepared

--- routes/admin/main.js
+++ routes/admin/main.js
@@ -1,12 +1,18 @@
 const express=require('express');
 const router = express.Router();
 
+router.all('/*',function(req,res,next){
+    req.app.locals.layout="admin"; // resetting defaultlayout to be admin when this route is run
+next();
+});
+
 
 router.get('/',function(req,res){
     res.render('admin/index');  //
 });
 router.get('/dashboard',function(req,res){
-    res.render('home/index');  //
+    res.render('admin/dashboard');  //
 });
+
 
 module.exports=router;
--- routes/home/main.js
+++ routes/home/main.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.all('/*',function(req,res,next){
+    req.app.locals.layout="home"; // resetting defaultlayout to be admin when this route is run
+    next();
+});
 
 router.get('/',function(req,res){
     res.render('home/index');  //
